6fae284ebc58ab0004533d680a8650dfaa28b541
TranVuHoang <tranvuhoangjr@gmail.com>
2026-04-27 22:51:48 +0700

    #31 Sử dụng biến với JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,10 +3,28 @@
 import "./style.css";
 
 const MyComponent = () => {
+  /** Kiểu dữ liệu nguyên thuỷ */
+  // const username = "Trang Trang"; // biến username kiểu string
+  const age = 23; // biến age kiểu number
+  const love = true; // biến love kiểu boolean
+
+  /** Kiểu dữ liệu object */
+  const array = [1, 2, 3];
+  const user = {
+    name: "Trang",
+    age: 25,
+  };
   return (
-    // Dùng Fragment <> </> để bọc tất cả phần tử HTML vào
     <>
-      <div className="">Trang xinh yêu love you</div>
+      <div className="">
+        {user.name} xinh yêu love you {age} {love}
+        <hr />
+        {JSON.stringify(user)}
+        <hr />
+        {JSON.stringify(array)}
+        <hr />
+        {console.log("Hoàng Trần")}
+      </div>
       <div className="chill" style={{ borderRadius: "10px" }}>
         Chill
       </div>
