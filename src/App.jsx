dc0dd0c4505ebf3ebcdd638e791ac60f337c51af
TranVuHoang <tranvuhoangjr@gmail.com>
2026-04-28 15:45:19 +0700

    #34 props

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,23 @@ import TodoData from "./components/todo/TodoData";
 import TodoNew from "./components/todo/TodoNew";
 
 const App = () => {
+  const username = "Hoàng IT 97 ";
+  const age = 29;
+  const data = {
+    address: "Hà Nội",
+    country: "Việt Nam",
+  };
   return (
     <div className="todo-container">
       <h1 className="todo-title">Todo list</h1>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={username}
+        age={age}
+        data={data}
+        address={data.address}
+        country={data.country}
+      />
       <div className="todo-image">
         <img src={TodoLogo} alt="React Logo" className="logo" />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,9 @@
-const TodoData = () => {
+const TodoData = (props) => {
+  // props là một object chứa tất cả các props được truyền vào component
+  console.log(">>> Props: ", props);
   return (
     <div className="todo-data">
+      <h2 className="name">Name: {props.name}</h2>
       <p className="desc">Learn React</p>
       <p className="desc">Lorem ipsum dolor sit amet.</p>
     </div>
